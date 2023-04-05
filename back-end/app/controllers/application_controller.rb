665d7e31c180a01b65f9917cdfefd5ff4c545e3e
Daniel Rice <danielrice5627@gmail.com>
2023-04-05 13:11:24 -0700

    mor ebackend CRUD

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  
+  #test
   get "/" do
     { message: "testing, testing, 123" }.to_json
   end
 
+  #takes in the username from a controlled form and finds associated user if there is one, returns the user or nil if failed
   get "/users/:username" do
    user = User.find_by(username: params[:username])
    user.to_json
@@ -21,4 +22,25 @@ class ApplicationController < Sinatra::Base
     user.to_json
   end
 
+  #takes in attribute hash as params and posts to wines table, returns created wine obj
+  post "/wines" do
+    wine = Wine.create(
+      name: params[:name],
+      rating: params[:rating],
+      notes: params[:notes],
+      user_id: params[:user_id]
+    )
+    wine.to_json
+  end
+
+  #takes in attribute hash as params and posts to beers table, returns created beer obj
+  post "/beers" do
+    beer = Beer.create(
+      name: params[:name],
+      rating: params[:rating],
+      notes: params[:notes],
+      user_id: params[:user_id]
+    )
+    beer.to_json
+  end
 end

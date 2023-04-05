57a62541c625cfecc0afe178e91f97b4f5f97732
Daniel Rice <danielrice5627@gmail.com>
2023-04-05 10:02:50 -0700

    fornt-end folder in correct place

---
+++ front-end/src/components/Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+function Home () {
+  
+  return (
+    <p></p>
+  );
+}
+
+export default Home;
---
+++ front-end/src/components/NewBeer.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+function NewBeer () {
+  
+  return (
+    <p></p>
+  );
+}
+
+export default NewBeer;

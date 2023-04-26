c5bf6e6d27096a8a8404227bff0307bd6e22b571
Daniel Rice <danielrice5627@gmail.com>
2023-04-25 20:41:40 -0700

    added functionality to newwine.js

--- front-end/src/components/NewWine.js
+++ front-end/src/components/NewWine.js
@@ -2,15 +2,43 @@ import React, { useState } from "react";
 
 function NewWine ({ user, setUser }) {
   
+  const [wineName, setWineName] = useState();
+  const [wineRating, setWineRating] = useState();
+  const [wineNotes, setWineNotes] = useState();
+
+  function postNewWine (event) {
+    event.preventDefault();
+
+    const formData = {
+      "name": wineName,
+      "rating": wineRating,
+      "notes" : wineNotes,
+      "user_id" : user.id
+      }; 
+    const configObj = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+      },
+      body: JSON.stringify(formData),
+      };
+
+      fetch(`http://localhost:9292/wines`, configObj)
+            .then(response => response.json())
+            .then(postedData => console.log(postedData))
+  }
+
+
   return (
     <div>
-        <form>
+        <form onSubmit={postNewWine}>
             <label for="wineName" >Wine Name:</label><br />
-            <input id="wineName" type="text"></input><br />
+            <input id="wineName" type="text" onChange={(e) => setWineName(e.target.value)}></input><br />
             <lable for="wineRating" >Rating:</lable><br />
-            <input id="wineRating" type="integer"></input><br />
+            <input id="wineRating" type="integer" onChange={(e) => setWineRating(e.target.value)}></input><br />
             <label for="wineNotes" >Notes:</label><br />
-            <textarea id="wineNotes" rows="4" columns="100"></textarea><br />
+            <textarea id="wineNotes" rows="4" columns="100" onChange={(e) => setWineNotes(e.target.value)}></textarea><br />
             <input type="submit" value="Submit"></input>
         </form>
     </div>
